a8e119c9c46422b8feface7b3afe88089ed2a898
Aitor Soto <aitorsotojimenez99@gmail.com>
2025-01-06 17:02:35 +0100

    Added new font and styles

--- src/components/DriverCard.tsx
+++ src/components/DriverCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./driverCard.css";
+import teamColors from "../data/teamColors.json";
 
 interface DriverCardProps {
   driver: {
@@ -7,6 +8,7 @@ interface DriverCardProps {
     code: string;
     forename: string;
     surname: string;
+    currentConstructor: string;
   };
 }
 
@@ -14,13 +16,24 @@ const DriverCard: React.FC<DriverCardProps> = ({ driver }) => {
   const getDriverCode = (code: string | null, surname: string) => {
     return code ? code : surname.slice(0, 3).toUpperCase();
   };
+
+  const getConstructorColor = (constructorName: string) => {
+    const team = teamColors.find((team) => team.name === constructorName);
+    return team ? team.color : "#000";
+  };
+
+  const constructorColor = getConstructorColor(driver.currentConstructor);
+
   return (
-    <div className="driver-card">
+    <div
+      className="driver-card "
+      style={{ "--constructor-color": constructorColor } as React.CSSProperties}
+    >
       <div className="driver-info">
-        <span className="driver-code">
+        <span className="driver-code f1-font-bold">
           {getDriverCode(driver.code, driver.surname)}
         </span>
-        <span className="driver-name">
+        <span className="driver-name f1-font">
           {driver.forename} {driver.surname}
         </span>
       </div>
